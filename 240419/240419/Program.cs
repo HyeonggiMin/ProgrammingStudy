d627fcd6c154abf4075b8c4027b2a5b5a58002ed
HyeonggiMin <167495767+HyeonggiMin@users.noreply.github.com>
2024-04-19 17:21:24 +0900

    feat: 4. 컬렉션 연습 + 도서관 책 찾기

--- 240419/240419/Program.cs
+++ 240419/240419/Program.cs
@@ -1,104 +1,90 @@
-﻿// 3. 배열 연습
+﻿// 4. 컬렉션 연습
 
-int number1;
-int number2;
-int number3;
+//배열: 고정형
+string[] names = { "민형기", "김현수", "양희문" };
 
-int[] numbers = new int[3]; // 배열의 크기를 정의
-
-// 배열의 초기화1
-numbers[0] = 1;
-numbers[1] = 2;
-numbers[2] = 3;
-
-int[] numbers2 = { 4, 5, 6 }; // 배열의 초기화2
-int[] numbers3 = new int[3] { 7, 8, 9 }; // 배열의 초기화3
-
-// 배열의 길이
-int length = numbers.Length;
-Console.WriteLine("배열의 길이: " + numbers.Length);
-
-numbers.Reverse(); // 배열을 역순으로
-
-//불가
-/*int[.] array1 = new int[3, 3]
+//리스트: 가변형
+List<string> names2 = new List<string>()
 {
-    numbers,
-    numbers2,
-    numbers3;
-};*/
-
-int[][] num = new int[3][]
-{
-    numbers, numbers2, numbers3
+    "민형기",
+    "김현수",
+    "양희문"
 };
-// int[][] num = {numbers, numbers2, numbers3};
-/* {1, 2, 3}
-   {4, 5, 6}
-   {7, 8, 9}*/
-Console.WriteLine(num[0][1]);
-Console.WriteLine(num[1][1]);
-Console.WriteLine(num[2][1]);
 
-// (3x3)배열의 2차원 선언
-int[,] coordinates = new int[3, 3]
+Console.WriteLine(names[0]);
+Console.WriteLine(names2[0]);
+names2.Add("장재원");
+Console.WriteLine(names2[3]);
+
+// 배열의 반복
+for(int i = 0; i < names.Length; i++)
 {
-    {1, 2, 3},
-    {4, 5, 6},
-    {7, 8, 9}
-};
-Console.WriteLine("0,1의 값: " + coordinates[0, 1]);
-Console.WriteLine("2,1의 값: " + coordinates[2, 1]);
-Console.WriteLine("배열의 길이: " + coordinates.Length);
-
-/* 실습1) 가로 3칸, 세로 2칸의 책꽂이에 문자열형 책 이름을 넣고,
-   2번째 쭐의 3번째 칸의 책이름을 출력한다.*/
-
-string[,] bookShelf =
-{
-    {"책1", "책2", "책3"},
-    {"책4", "책5", "책6"}
-};
-bookShelf[1, 2] = "책60";
-Console.WriteLine("1,2의 책 이름: " + bookShelf[1, 2]);
-
-// 실습2) 두 개의 숫자, 연산자를 입력을 받아 4칙 연산 계산기 만들기
-/*string strA = Console.ReadLine();
-int numberA = int.Parse(strA); // String을 문자열로 변환
-Console.WriteLine(name);*/
-
-// 입력 예시
-// 3
-// +
-// 5
-// 결과 = 8
-
-Console.WriteLine("첫 번째 숫자를 입력해 주세요.");
-string strA = Console.ReadLine();
-int numberA = int.Parse(strA);
-
-Console.WriteLine("연산자를 입력해 주세요.(+,-,*./)");
-string oper = Console.ReadLine();
-
-Console.WriteLine("두 번째 숫자를 입력해 주세요.");
-string strB = Console.ReadLine();
-int numberB = int.Parse(strB);
-float result = 0;
-
-switch (oper)
-{
-    case "+":
-        result = numberA + numberB;
-        break;
-    case "-":
-        result = numberA - numberB;
-        break;
-    case "*":
-        result = numberA * numberB;
-        break;
-    case "/":
-        result = numberA / (float)numberB; //명시적 형 변환
-        break;
+    Console.WriteLine(names[i]);
 }
 
-Console.WriteLine("결과는:" + result + "입니다.");
+// 리스트의 반복
+for(int i = 0;i < names2.Count; i++)
+{
+    Console.WriteLine(names2[i]);
+}
+
+// 리스트에서 이름 찾기
+int index = names2.IndexOf("양희문");
+Console.WriteLine("양희문은 " + index + "번째에 있습니다.");
+
+// 존재 여부 확인
+bool isExist = names2.Contains("민형기");
+if (isExist)
+    Console.WriteLine("존재합니다");
+
+//리스트를 정렬
+names2.Sort();
+for(int i = 0; i < names2.Count; i++)
+{
+    Console.WriteLine(names2[i]);
+}
+
+//딕셔너리: "example"(key), 5번째(value)
+Dictionary<string,int> dicionary = new Dictionary<string, int>();
+dicionary.Add("book", 0);
+dicionary.Add("computer", 1);
+dicionary.Add("mouse", 2);
+
+index = dicionary["mouse"];
+Console.WriteLine("mouse는 " + index + "번째에 있습니다.");
+bool isContain = dicionary.ContainsKey("book");
+if (isContain)
+    Console.WriteLine("book이 존재합니다.");
+
+// 실습
+// 1.도서관에 가로 3칸, 세로 2층의 책꽂이가 있습니다.
+// (미리 책꽂이에 책들을 꽂아놓은 상태)
+// 2. 특정 책의 이름을 입력받아 책의 일련번호(위치)를
+// 출력하는 책꽂이 시스템을 만드세요.
+
+// 예시
+// 도서의 이름을 입력해 주세요.
+// _
+// 도서의 위치는 (3,2) 입니다.
+
+// 오른쪽 버튼을 눌러주세요. 등록(0), 검색(1), 제거(2)
+// 0
+// 삼국지
+// 3,5
+// 오른쪽 버튼을 눌러주세요. 등록(0), 검색(1), 제거(2)
+// 1
+// 삼국지
+// 3,5
+
+Dictionary<string, string> library = new Dictionary<string, string>();
+library.Add("책1", "1,1");
+library.Add("책2", "1,2");
+library.Add("책3", "1,3");
+library.Add("책4", "2,3");
+library.Add("책5", "2,3");
+library.Add("책6", "2,3");
+
+Console.WriteLine("도서의 이름을 입력해 주세요.");
+string bookName = Console.ReadLine();
+string location = library[bookName];
+Console.WriteLine("도서의 위치는 " + location + "입니다.");
--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbebc29197be031e0b88cfbb4058c8760808354c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e93132edcbddf24ea9bf01894508824b0311b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
