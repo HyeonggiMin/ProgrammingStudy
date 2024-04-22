144a92f5e31ef4751682f67037b6dcd958756da2
HyeonggiMin <167495767+HyeonggiMin@users.noreply.github.com>
2024-04-22 12:35:15 +0900

    feat: 5. 컬렉션 연습과 실습
    Stack, Queue

--- 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
+++ 240419/240419/obj/Debug/net8.0/240419.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("240419")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78e93132edcbddf24ea9bf01894508824b0311b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+352e15cb7ac731c7d9ce456538b8a3f6897325b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("240419")]
 [assembly: System.Reflection.AssemblyTitleAttribute("240419")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
